b3c7945651657157fa777c1e5f62916402db1903
Emmanuel Jaep <emmanuel.jaep@epfl.ch>
2017-11-25 10:12:04 +0100

    initial commit

---
+++ taurus.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+TIMESTAMP=$(date +"%Y%m%d_%H%M%S")
+echo "Please provide session description (a-z, A-Z, - and _ characters accepted):"
+read SESSIONDESCRIPTION
+echo Please provide the config file to run:
+read CONFIGFILE
+
+mkdir -p results/$SESSIONDESCRIPTION/$TIMESTAMP/artifacts
+mkdir -p results/$SESSIONDESCRIPTION/$TIMESTAMP/tests
+
+cp -r tests/ results/$SESSIONDESCRIPTION/$TIMESTAMP/tests
+
+docker run \
+    -i \
+    --rm \
+    --net="host" \
+    -v $(pwd)/results/$SESSIONDESCRIPTION/$TIMESTAMP/tests:/bzt-configs \
+    -v $(pwd)/results/$SESSIONDESCRIPTION/$TIMESTAMP/artifacts:/tmp/artifacts \
+    -v $(pwd)/results/$SESSIONDESCRIPTION/$TIMESTAMP:/tmp/results \
+    blazemeter/taurus $CONFIGFILE
