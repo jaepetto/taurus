7d2a8d26631c3fed10733e7a376fc90d094f489e
Team <team@idevampc2>
2017-11-25 10:48:34 +0100

    fixed network access to docker container

--- taurus.sh
+++ taurus.sh
@@ -13,7 +13,6 @@ cp -r tests/* results/$SESSIONDESCRIPTION/$TIMESTAMP/tests
 docker run \
     -i \
     --rm \
-    --net="host" \
     -v $(pwd)/results/$SESSIONDESCRIPTION/$TIMESTAMP/tests:/bzt-configs \
     -v $(pwd)/results/$SESSIONDESCRIPTION/$TIMESTAMP/artifacts:/tmp/artifacts \
     -v $(pwd)/results/$SESSIONDESCRIPTION/$TIMESTAMP:/tmp/results \
