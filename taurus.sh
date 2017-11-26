52bb9ed8323cfb3f7c68c01fe4f83c23ed3cbf6f
Emmanuel Jaep <emmanuel.jaep@epfl.ch>
2017-11-26 10:10:32 +0100

    Added concurrency parameter

--- taurus.sh
+++ taurus.sh
@@ -4,6 +4,8 @@ echo "Please provide session description (a-z, A-Z, - and _ characters accepted)
 read SESSIONDESCRIPTION
 echo Please provide the config file to run:
 read CONFIGFILE
+echo Please provide the concurrency:
+read CONCURRENCY
 
 mkdir -p results/$SESSIONDESCRIPTION/$TIMESTAMP/artifacts
 mkdir -p results/$SESSIONDESCRIPTION/$TIMESTAMP/tests
@@ -16,4 +18,4 @@ docker run \
     -v $(pwd)/results/$SESSIONDESCRIPTION/$TIMESTAMP/tests:/bzt-configs \
     -v $(pwd)/results/$SESSIONDESCRIPTION/$TIMESTAMP/artifacts:/tmp/artifacts \
     -v $(pwd)/results/$SESSIONDESCRIPTION/$TIMESTAMP:/tmp/results \
-    blazemeter/taurus $CONFIGFILE
+    blazemeter/taurus -o execution.concurrency=$CONCURRENCY $CONFIGFILE
